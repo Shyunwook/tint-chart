b810188e54c5c4d61c7af320ad3b269e405261e0
admin <admin@MAC-9852-0000.local>
2019-03-06 10:21:35 +0900

    3월 데이터 업로드 오류로 data 다시 세팅 버전

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ let cron = require('node-cron');
 let request = require('request');
 
 let redis = require('redis');
-let cacheClient = redis.createClient(6379,'172.31.29.112');
-// let cacheClient = redis.createClient(6379,'127.0.0.1');
+// let cacheClient = redis.createClient(6379,'172.31.29.112');
+let cacheClient = redis.createClient(6379,'127.0.0.1');
 
-const FUNC = require('./func.js')();
+const FUNC = require('./common.js')();
+const REDIS = require('./redis-func.js')();
 
 const wrap = asyncFn => {
 // FIXME: Promise와 catch를 이용하면 더 간결해질 것 같습니다.
@@ -38,23 +39,43 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.get('/',(req, res) => {
   res.render('index.ejs',{result : "", other_grouped_weight_data : "", gs_grouped_weight_data : ""});
 });
+//
+// app.get('/test',(req, res) => {
+//   let period = {
+//   "dateFrom": "2018-12-31",
+//   "dateTo": "2018-12-31"
+// };
+//   return new Promise((resolve, reject) => {
+//     let url = "https://fy2b0csnq7.execute-api.us-west-2.amazonaws.com/prod/vaccine-c-api";
+//     request({
+//       url : url,
+//       method : 'POST',
+//       body : period,
+//       json : true,
+//       encoding : null
+//     },(error, response, data) => {
+//       if(error){
+//         console.error(error);
+//         reject(Error('something is wrong -> getSchedule'));
+//       }else if(response.statusCode === 200){
+//         let result = JSON.stringify(data);
+//         resolve(data);
+//       }else{
+//         console.log(response.statusCode);
+//         reject(Error('wrong status code...'));
+//       }
+//     })
+//   }).then((val) => {
+//     console.log(val);
+//   })
+// })
 
-app.get('/getData',(req, res) => {
-  let period = {dateFrom : "2019-02-12", dateTo : "2019-02-12"};
-  FUNC.getDynamoData(period).then((data) => {
-    // res.send({result : data.result, other_grouped_weight_data : data.other_grouped_weight_data, gs_grouped_weight_data : data.gs_grouped_weight_data});
-  }, (error) => {
-    res.render('error.ejs');
-  });
-});
-
-app.post('/getRedisData', wrap(async(req, res) => {
+app.post('/getScheduleData', wrap(async(req, res) => {
   let period = {dateFrom : req.body.dateFrom, dateTo : req.body.dateTo};
-  // let period = {dateFrom : '2018-11-13', dateTo : '2018-11-16'};
   let raw = {};
   let initial_day = "";
 
-  let flag = await (() => {
+  let redis_data_flag = await (() => {
     return new Promise((resolve, reject) => {
       cacheClient.sort("dateList","alpha",(err,result) => {
         initial_day = result[0];
@@ -67,7 +88,7 @@ app.post('/getRedisData', wrap(async(req, res) => {
     })
   })();
 
-  if(flag){
+  if(redis_data_flag){
     raw = await FUNC.getRedisData(period,cacheClient);
     console.log("Redis only!!");
   }else{
@@ -77,43 +98,35 @@ app.post('/getRedisData', wrap(async(req, res) => {
     let redis_raw = await FUNC.getRedisData(period,cacheClient);
     let dynamo_raw = await FUNC.getDynamoData(dynamo_period);
 
-    // let redis_period = { dateFrom : initial_day, dateTo : '2018-11-16' };
-    // let dynamo_period = { dateFrom : '2018-11-13', dateTo : moment(initial_day).add(-1,'days').format("YYYY-MM-DD")};
-
     raw = [...redis_raw, ...dynamo_raw];
     console.log("DynamoDB + Redis!!");
   }
 
   FUNC.setScheduleData(period,raw).then((result) => {
     res.send(result);
-    // res.render('index.ejs',{result : JSON.stringify(result.result), other_grouped_weight_data : JSON.stringify(result.other_grouped_weight_data), gs_grouped_weight_data : JSON.stringify(result.gs_grouped_weight_data)});
-    // res.send({result : result.result, other_grouped_weight_data : result.other_grouped_weight_data, gs_grouped_weight_data : result.gs_grouped_weight_data});
   });
 }));
 
 
-
-
-//Redis data handling API
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// app.get('/redis-load-data',(req, res) => {
-//   let period_list = FUNC.setPeriodList(90);
-//   FUNC.loadDataToRedis(period_list, cacheClient, res);
-// });
-
-// app.get('/flushall',(req, res) => {
-//   res.send(cacheClient.flushall());
-// });
-
 cron.schedule('0 0 4 * * *',() => {
   let today = moment().add(0,'days').format("YYYY-MM-DD");
   let period = [{dateFrom : today, dateTo : today}];
-  FUNC.loadDataToRedis(period, cacheClient);
+  REDIS.loadDataToRedis(period, cacheClient);
 })
-
 
 
 app.listen('8081',function(){
   console.log('app listening on port 8081');
 })
+
+
+
+//Redis data handling API / 처음 레디스 데이터 세팅 API-----------------------------------------------------------------------------------------------
+// app.get('/flushall',(req, res) => {
+//   res.send(cacheClient.flushall());
+// });
+
+// app.get('/redis-load-data',(req, res) => {
+//   let period_list = REDIS.setPeriodList(90);
+//   REDIS.loadDataToRedis(period_list, cacheClient, res);
+// });
